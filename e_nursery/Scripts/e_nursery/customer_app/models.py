23bcbaddba6d37ca6c18fd7ae5039cd3faea3389
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-06-28 13:12:22 +0530

    jwt authentication
    authentication using pyJWT

--- e_nursery/Scripts/e_nursery/customer_app/models.py
+++ e_nursery/Scripts/e_nursery/customer_app/models.py
@@ -1,20 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# created by Priya Patel & Samundar Singh Rathore
-# Create your models here.
 
-# Model for Customer
-class Customer(models.Model):
-    id=models.AutoField(primary_key=True,auto_created=True)
-    first_name=models.CharField(max_length=255)
-    last_name=models.CharField(max_length=255)
-    email=models.EmailField()
-    password=models.CharField(max_length=255)
-    role=models.CharField(max_length=255)
-    
-    # for the database table
+class Customer(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+
+    # Exclude unwanted fields from the model
+    is_superuser = None
+    is_staff = None
+    is_active = None
+    date_joined = None
+    last_login=None
+
     class Meta:
         db_table = "customer"
+
+    # Set the username field to None
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password']
 
 #Model for Users
 class Users(models.Model):
--- e_nursery/Scripts/e_nursery/customer_app/serializer.py
+++ e_nursery/Scripts/e_nursery/customer_app/serializer.py
@@ -1,12 +1,23 @@
-# created by Priya Patel & Samundar Singh Rathore
 from rest_framework import serializers
 from .models import *
 
-# for the database table & serialize the object
-class CustomerModelSerializer(serializers.ModelSerializer):
+class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Customer
-        fields='__all__' #include all the fields of customer model
+        model = Customer
+        fields = ('id', 'first_name', 'last_name', 'email', 'password', 'role')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        print(instance)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 
 #for Users table 
 class UserSerializer(serializers.ModelSerializer):
