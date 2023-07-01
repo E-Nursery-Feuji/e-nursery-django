eecde649c781e70f8582e4c48c0b1e00b19f2efa
MaheshMuthineni <muthinenimahesh143@gmail.com>
2023-07-01 23:09:30 +0530

    ....

--- e_nursery/Scripts/e_nursery/admin_app/serializer.py
+++ e_nursery/Scripts/e_nursery/admin_app/serializer.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
 from .models import *
+
 class AdminModelSerializer(serializers.ModelSerializer):
     class Meta:
         model=Admin
-        fields='__all__'
+        fields = ('id', 'first_name', 'last_name', 'email', 'password','role','status')
+
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        print(instance)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- e_nursery/Scripts/e_nursery/admin_app/views.py
+++ e_nursery/Scripts/e_nursery/admin_app/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializer import *
+from customer_app.models import *
 import logging as log
 from rest_framework import status
 
@@ -25,16 +26,14 @@ def registerAdmin(request):
         log.info("register_customer:data is valid")
         email=request.data.get('email') #get email from request data
         log.info("Checking Email exits")
-        data_exist=Admin.objects.filter(email=email).exists() #check email is present or not
-
+        data_exist=Users.objects.filter(email=email).exists() #check email is present or not
         if data_exist:
             log.info("Email exist")
             return Response("present") #email present then reponse
         else:
             log.info("Email not exist")
-
             serilizer.save()  #save the data into databse
-            log.info("customer data is saved")
+            log.info("Admin data is saved")
             return Response(serilizer.data)  #return this response to frontend
     else:
         log.info("data is invalid")
