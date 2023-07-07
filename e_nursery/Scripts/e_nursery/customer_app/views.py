55d8a929ac6516c960f0ef4a3037c1b32b403ec9
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-07-07 14:04:51 +0530

    blogimage
    store some blog images

--- e_nursery/Scripts/e_nursery/customer_app/views.py
+++ e_nursery/Scripts/e_nursery/customer_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-# from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import *
 from .serializer import *
 import logging as log
