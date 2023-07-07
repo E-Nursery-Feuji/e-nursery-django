1710aa37c88af3bd664876d455816aa3951fa93a
MaheshMuthineni <129718045+MaheshMuthineni@users.noreply.github.com>
2023-07-07 11:35:40 +0530

    lsdjs (#38)

--- e_nursery/Scripts/e_nursery/customer_app/email.py
+++ e_nursery/Scripts/e_nursery/customer_app/email.py
@@ -9,13 +9,13 @@ log.basicConfig(filename='e_nursery_log.log', level=log.DEBUG, format='%(asctime
 # for send otp
 def send_opt_via_email(email):
     log.info("Send_otp_via_mail:starts")
-    subject="forget password otp" #subject for email
+    subject="OTP for forgot password" #subject for email
     otp=random.randint(100000,999999) #otp genrate
-    log.info("OTP genrated")
-    message=f"Your opt is : {otp}" #message for email
+    log.info("OTP generated")
+    message=f"You have requested for password reset. Your OTP is : {otp}. This OTP is valid for limited time" #message for email
     log.info(otp)
     email_from=settings.EMAIL_HOST #host email
     log.info(otp)
     send_mail(subject,message,email_from,[email]) #the send mail fiunction
-    log.info("Email send")
+    log.info("Email sent")
     return otp
