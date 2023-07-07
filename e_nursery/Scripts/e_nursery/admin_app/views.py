cf8690b4ca54f00505d97d3faa9065e7d95fa692
MaheshMuthineni <muthinenimahesh143@gmail.com>
2023-07-07 11:04:24 +0530

    hw

--- e_nursery/Scripts/e_nursery/admin_app/views.py
+++ e_nursery/Scripts/e_nursery/admin_app/views.py
@@ -20,26 +20,31 @@ log.basicConfig(filename='e_nursery_log.log', level=log.DEBUG, format='%(asctime
 
 @api_view(['POST'])
 def registerAdmin(request):
-    log.info("register_customer function started")
-    serilizer=AdminModelSerializer(data=request.data)  #serializer the customer data
-    if serilizer.is_valid():  #checking the validation of serialized data
-        log.info("register_customer:data is valid")
-        email=request.data.get('email') #get email from request data
-        log.info("Checking Email exits")
-        data_exist=Users.objects.filter(email=email).exists() #check email is present or not
-        if data_exist:
-            log.info("Email exist")
-            return Response("present") #email present then reponse
-        else:
-            log.info("Email not exist")
-            serilizer.save()  #save the data into databse
-            log.info("Admin data is saved")
-            return Response(serilizer.data)  #return this response to frontend
-    else:
-        log.info("data is invalid")
-        return Response("present")
-    
-    
+    log.info("register_admin function started")
+    try:  
+   
+            serilizer=AdminModelSerializer(data=request.data)  #serializer the admin data
+            if serilizer.is_valid():  #checking the validation of serialized data
+                log.info("register_admin:data is valid")
+                email=request.data.get('email') #get email from request data
+                log.info("Checking Email exits")
+                data_exist=Users.objects.filter(email=email).exists() #check email is present or not
+                if data_exist:
+                    log.info("Email exist")
+                    return Response("present") #email present then reponse
+                else:
+                    log.info("Email not exist")
+                    serilizer.save()  #save the data into databse
+                    log.info("Admin data is saved")
+                    return Response(serilizer.data)  #return this response to frontend
+            else:
+                log.info("data is invalid")
+                return Response("present")
+    except Exception as e:
+       return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
+
+
+
 @api_view(['GET'])
 def getAdmin(request, id=None):
     if id is not None:
@@ -56,41 +61,50 @@ def getAdmin(request, id=None):
         return Response(serializer.data)
     
 
-@api_view(['PUT'])
+@api_view(['PATCH'])
 def update(request, id):
     try:
         admin = Admin.objects.get(id=id)
+        log.info("################################")
         log.info(admin)
         serializer = AdminModelSerializer(admin, data=request.data)
-        log.info(request.data)
-        if serializer.is_valid():
-            serializer.save()
+        log.info(serializer)
+        log.info("****************************************")
+        admin.id=request.data.get('id')
+        admin.first_name=request.data.get('first_name')
+        admin.last_name=request.data.get('last_name')
+        admin.email=request.data.get('email')
+        try:                       
+            admin.save()        # Save the updated Admin object
+            serializer = AdminModelSerializer(admin)  # Return the serialized data in the response
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+          
     except Admin.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['PATCH'])
 def status(request, id):
-    log.info("Enter into insert Schedule")
+    log.info("Enter into insert admin")
     try:
-        schedule = Admin.objects.get(id=id)
+        admin = Admin.objects.get(id=id)
     except Admin.DoesNotExist:
-        return Response({'error': 'Schedule not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'admin not found'}, status=status.HTTP_404_NOT_FOUND)
 
     # Assign the field value "InActive" to the desired field
 
-    if (schedule.status == "Active"):
-        schedule.status = "InActive"
-    elif (schedule.status == "InActive"):
-        schedule.status = "Active"
+    if (admin.status == "Active"):
+        admin.status = "InActive"
+    elif (admin.status == "InActive"):
+        admin.status = "Active"
     else:
-        schedule.status = "Active"
+        admin.status = "Active"
 
                         
-    try:                   # Save the updated Schedule object
-        schedule.save()    # Return the serialized data in the response
-        serializer = AdminModelSerializer(schedule)
+    try:                   # Save the updated Admin object
+        admin.save()       # Return the serialized data in the response
+        serializer = AdminModelSerializer(admin)
         return Response(serializer.data)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
